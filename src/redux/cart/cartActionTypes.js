5d764599dee3af8518b91382162d949333c7953c
Joaopnobre <118943974+Joaopnobre@users.noreply.github.com>
2023-11-21 07:16:40 +0000

    web-icon

--- src/redux/cart/cartActionTypes.js
+++ src/redux/cart/cartActionTypes.js
@@ -1,8 +1,8 @@
 const CartActionTypes = {
     ADD_PRODUCT: 'cart/addProduct',
     REMOVE_PRODUCT: 'cart/removeProduct',
-    INCREASE_PRODUCT: 'cart/increaseProduct',
-    DECREASE_PRODUCT: 'cart/decreaseProduct',
+    INCREASE_PRODUCT: 'cart/increaseProductQuantity',
+    DECREASE_PRODUCT: 'cart/decreaseProductQuantity',
 };
 
 export default CartActionTypes;
